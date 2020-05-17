7acc836114e8c1e23e6e68f0fe65c98030b70c5d
AnnaPS <anna-91_@hotmail.com>
2020-05-17 20:06:19 +0200

    container and safe area widget example

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => MyApp();
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.blue,
+        body: SafeArea(
+          child: Container(
+            height: 100,
+            width: 100,
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.only(left: 20),
+            color: Colors.white,
+            child: Text("hello"),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
